c6bd3258ca80fdf9a8b8aff889d2d1f31f86d457
wildfireOfMine <iamtheanguera@gmail.com>
2025-03-04 14:31:05 +0100

    Added static root

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'upload/'

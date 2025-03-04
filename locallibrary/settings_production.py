f97cd0dea2049e3df457790f11b2706b25aae762
wildfireOfMine <iamtheanguera@gmail.com>
2025-03-04 13:18:48 +0100

    Please, work

---
+++ locallibrary/settings_production.py
@@ -0,0 +1,13 @@
+from decouple import config, Csv
+ALLOWED_HOSTS = ['.vercel.app']
+DEBUG = False
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": 5432,
+        "NAME": config("POSTGRES_DATABASE"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD")
+    }
+}
